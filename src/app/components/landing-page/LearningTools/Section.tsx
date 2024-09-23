2f455208b054ea9b7d139456235b396d61cff038
haroon sadiq <muhammadharoon9803@gmail.com>
2024-09-24 01:13:50 +0500

    merged

--- src/app/components/landing-page/LearningTools/Section.tsx
+++ src/app/components/landing-page/LearningTools/Section.tsx
@@ -12,7 +12,7 @@ const Section: React.FC<SectionProps> = ({ title, cards }) => {
   const marginTopStyles = title === 'Flexible Scheduling' ? 'mt-14 lg:mt-24' : 'lg:mt-0';
 
   return (
-    <div className={`flex flex-col justify-start ${marginTopStyles} lg:px-12 xl:px-24 space-y-8 w-full lg:w-1/2`}>
+    <div className={`flex flex-col justify-start ${marginTopStyles} lg:px-12  space-y-8 w-full lg:w-1/2`}>
       <h2 className="text-4xl font-bold text-white mb-8 text-center lg:text-left">{title}</h2>
       <CardGrid cards={cards} />
     </div>
