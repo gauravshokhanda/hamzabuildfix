eee9a1fb04f05dbfa7763094fda89b34780f53cd
Shoaib Imran <Shoaib.Imran33@thehutgroup.com>
2024-10-23 01:03:33 +0100

    teacher dashboard fix

--- client/src/app/teacher/page.tsx
+++ client/src/app/teacher/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MainDashboard from "../components/dashboard/main-dashboard/MainDashboard";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <MainDashboard />
+    </div>
+  );
 };
 
 export default page;
